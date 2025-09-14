7a9ece087459c387559aacd6e9806e9c89376d93
Srishti-Doshi <srishtidoshi7@gmail.com>
2025-09-14 17:53:37 +0530

    TOC Practical 1: Count 0s and 1s in binary string

---
+++ 5th_Semester/TOC/Practical1.c
@@ -0,0 +1,28 @@
+// Write a program to count number of 0 s and 1 s in a given string(without using Libraries)
+#include <stdio.h>
+
+int main()
+{
+    char str[] = "110011";
+    printf("Given String is %s\n", str);
+    int length = 0, zero = 0, one = 0;
+
+    while (str[length] != '\0')
+    {
+        length += 1;
+    }
+    for (int i = 0; i < length; i++)
+    {
+        if (str[i] == '0')
+        {
+            zero += 1;
+        }
+        if (str[i] == '1')
+        {
+            one += 1;
+        }
+    }
+    printf("The number of 0 in given binary string is %d\n", zero);
+    printf("The number of 1 in given binary string is %d\n", one);
+    return 0;
+}
