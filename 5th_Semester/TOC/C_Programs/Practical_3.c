7b9af0dfafaf70bc0e4ed072d6e8549047066349
Srishti-Doshi <srishtidoshi7@gmail.com>
2025-10-06 12:30:24 +0530

    Add DFA program that accepts exactly one '0'

---
+++ 5th_Semester/TOC/C_Programs/Practical_3.c
@@ -0,0 +1,48 @@
+// 3) Implement a DFA in C program that accepts all strings containing exactly one '0' over alphabet{0,1}.
+#include <stdio.h>
+
+int main()
+{
+    char state = 'A';
+    char str[100];
+
+    printf("Enter a binary string: ");
+    scanf("%s", str);
+
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] != '0' && str[i] != '1')
+        {
+            printf("Invalid input! Only 0 and 1 are allowed.\n");
+            return 0;
+        }
+
+        switch (state)
+        {
+        case 'A':
+            if (str[i] == '0')
+                state = 'B';
+            else
+                state = 'A';
+            break;
+
+        case 'B':
+            if (str[i] == '0')
+                state = 'T';
+            else
+                state = 'B';
+            break;
+
+        case 'T':
+            state = 'T';
+            break;
+        }
+    }
+
+    if (state == 'B')
+        printf(" String Accepted(contains exactly one '0').\n");
+    else
+        printf("String Rejected.");
+
+    return 0;
+}
