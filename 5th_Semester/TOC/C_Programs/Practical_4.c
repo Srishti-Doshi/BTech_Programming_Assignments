f9658682f10b2893e007b6ac78e8cfab0885f978
Srishti-Doshi <srishtidoshi7@gmail.com>
2025-10-06 12:32:26 +0530

    Add DFA program that accepts at least one '0'

---
+++ 5th_Semester/TOC/C_Programs/Practical_4.c
@@ -0,0 +1,42 @@
+// 4) Implement a DFA in C program that accepts all strings containing at least one '0' over alphabet {0,1}.
+
+#include <stdio.h>
+
+int main()
+{
+    char state = 'A';
+    char str[100];
+
+    printf("Enter a binary string: ");
+    scanf("%s", str);
+
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] != '0' && str[i] != '1')
+        {
+            printf("Invalid String! Only 0 and 1 are allowed.\n");
+            return 0;
+        }
+
+        switch (state)
+        {
+        case 'A':
+            if (str[i] == '0')
+                state = 'B';
+            else
+                state = 'A';
+            break;
+
+        case 'B':
+            state = 'B';
+            break;
+        }
+    }
+
+    if (state == 'B')
+        printf("String Accepted.\n");
+    else
+        printf("String Rejected.\n");
+
+    return 0;
+}
