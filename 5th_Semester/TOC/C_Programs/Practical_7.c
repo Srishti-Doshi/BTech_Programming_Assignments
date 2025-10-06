33b3ce22b0cc83dfd160581374bd2dc1c7fda392
Srishti-Doshi <srishtidoshi7@gmail.com>
2025-10-06 15:23:47 +0530

    Add DFA program for three consecutive ones

--- 5th_Semester/TOC/C_Programs/Practical_7.c
+++ 5th_Semester/TOC/C_Programs/Practical_7.c
@@ -1,55 +1,72 @@
-/* 7) Implement a DFA in C program that accepts all strings divisible by 3 over alphabet {0,1}.
-       or
-   Design a program for creating a machine that accepts 2 Mod 3.
-*/
+// 4) Implement a DFA in C program that accepts all strings containing three consecutive ones over alphabet {0,1}.
 
 #include <stdio.h>
 
 int main()
 {
-        char state = 'A';
-        char str[100];
+    char state = 'A';
+    char str[100];
 
-        printf("Enter a binary string: ");
-        scanf("%s", str);
+    printf("Enter a Binary String: ");
+    scanf("%s", str);
 
-        for (int i = 0; str[i] != '\0'; i++)
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] != '0' && str[i] != '1')
         {
-                if (str[i] != '0' && str[i] != '1')
-                {
-                        printf("Invalid input! Only binary numbers are allowed.\n");
-                        return 0;
-                }
-
-                switch (state)
-                {
-                case 'A':
-                        if (str[i] == '0')
-                                state = 'A';
-                        else
-                                state = 'B';
-                        break;
-
-                case 'B':
-                        if (str[i] == '0')
-                                state = 'C';
-                        else
-                                state = 'A';
-                        break;
-
-                case 'C':
-                        if (str[i] == '0')
-                                state = 'B';
-                        else
-                                state = 'C';
-                        break;
-                }
+            printf("Invalid Input! Only binary digits (0 and 1) are allowed.\n");
+            return 0;
         }
 
-        if (state == 'A')
-                printf("String Accepted (divisible by 3).\n");
-        else
-                printf("String Rejected.\n");
+        switch (state)
+        {
+        case 'A':
+            if (str[i] == '1')
+            {
+                state = 'B';
+            }
+            else
+            {
+                state = 'A';
+            }
+            break;
 
-        return 0;
+        case 'B':
+            if (str[i] == '1')
+            {
+                state = 'C';
+            }
+            else
+            {
+                state = 'A';
+            }
+            break;
+
+        case 'C':
+            if (str[i] == '1')
+            {
+                state = 'D';
+            }
+            else
+            {
+                state = 'A';
+            }
+            break;
+
+        case 'D':
+            state = 'D';
+            break;
+        }
+    }
+
+    if (state == 'D')
+    {
+        printf("String Accepted (contains three consecutive 1's).\n");
+    }
+    else
+    {
+        printf("String Rejected (does not contain three consecutive 1's).\n");
+    }
+
+    return 0;
 }
